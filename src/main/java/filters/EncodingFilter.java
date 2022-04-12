8361dc22a8d0707ac2cfafb9d52bc9697668dc9f
自分の名前 <>
2022-04-12 13:33:07 +0900

    NewServlet.javaを作成

---
+++ src/main/java/filters/EncodingFilter.java
@@ -0,0 +1,35 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+
+    public EncodingFilter() {
+        super();
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // place your code here
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        // pass the request along the filter chain
+        chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
